18a2ff2c8ae3cda9b1227a5d4000f113be472b33
Felipe Ortega <felipeortegabustamante@gmail.com>
2018-02-12 15:33:34 -0300

    Improved watering

--- watering.ino
+++ watering.ino
@@ -2,8 +2,8 @@
 #define READ_INTERVAL 300000 //Milis between read
 #define READ_DELAY 25 //Milis before read sensor
 #define WATER_DELAY 10000 //Milis the relay is turned on
-#define WATER_INTERVAL 290000 //Milis between watering and checking sensor
-#define SENSOR_LOW 300 //If watering and sensor goes below this, watering is done.
+#define WATER_MUTE 4
+#define SENSOR_LOW 400 //If watering and sensor goes below this, watering is done.
 #define SENSOR_HIGH 600 //If sensor goes up this, watering is need
 #define NO_SENSOR 899 //If sensors goes up this, sensor is off the soil. Prevent infinite watering.
 #define NO_WATER_NEED -1
@@ -13,69 +13,86 @@
 struct Pot {
   int PowerSensor;
   int DataSensor;
+  int WaterMute;
+  boolean Watering;
   int PowerRelay;
   int Value; } pots[NUM];
 
-int i;
+int i, n;
 
 void UpdateValues(){
   for(i = 0; i < NUM; i++){
-    UpdateValue();
-  }
-}
+    digitalWrite(pots[i].PowerSensor, HIGH);
+    delay(READ_DELAY);
+    pots[i].Value = analogRead(pots[i].DataSensor);
+    digitalWrite(pots[i].PowerSensor, LOW);
 
-void UpdateValue(){
-  digitalWrite(pots[i].PowerSensor, HIGH);
-  delay(READ_DELAY);
-  pots[i].Value = analogRead(pots[i].DataSensor);
-  digitalWrite(pots[i].PowerSensor, LOW);
-  
-  #ifdef LOGGING
-  Serial.print(i);
-  Serial.print(",UPDATE,");
-  Serial.println(pots[i].Value);
-  #endif
+    #ifdef LOGGING
+    Serial.print(i);
+    Serial.print(",UPDATE,");
+    Serial.println(pots[i].Value);
+    #endif
+  }
 }
 
 void NeedWater(){
   for(i = 0; i < NUM; i++){
-    if(NO_SENSOR < pots[i].Value){
-      #ifdef LOGGING
-      Serial.print(i);
-      Serial.print(",NOSENSOR,");
-      Serial.println(pots[i].Value);
-      #endif
-      continue;
-    }else if(SENSOR_HIGH < pots[i].Value){
-      #ifdef LOGGING
-      Serial.print(i);
-      Serial.print(",NEEDWATER,");
-      Serial.println(pots[i].Value);
-      #endif
-      return;
+    if(pots[i].Watering){
+      if(pots[i].WaterMute == 0){
+        pots[i].Watering = pots[i].Value > SENSOR_LOW; //if Watering and WaterMute == 0, pot is watered
+        if(pots[i].Watering == false){
+          Serial.print(i);
+          Serial.print(",DONEWATER,");
+          Serial.println(pots[i].Value);
+        }
+      }else{
+        pots[i].WaterMute--;
+      }
+    }else{
+      if(NO_SENSOR < pots[i].Value){
+        #ifdef LOGGING
+        Serial.print(i);
+        Serial.print(",NOSENSOR,");
+        Serial.println(pots[i].Value);
+        #endif
+      }else if(SENSOR_HIGH < pots[i].Value){
+        #ifdef LOGGING
+        Serial.print(i);
+        Serial.print(",NEEDWATER,");
+        Serial.println(pots[i].Value);
+        #endif
+        pots[i].Watering = true;
+      }
     }
   }
-  i = NO_WATER_NEED;
 }
 
-void WaterPot(){
-  do{
-    #ifdef LOGGING
-    Serial.print(i);
-    Serial.print(",WATERING,");
-    Serial.println(pots[i].Value);
-    #endif
-    
-    digitalWrite(pots[i].PowerRelay, LOW);
-    delay(WATER_DELAY);
-    digitalWrite(pots[i].PowerRelay, HIGH);
-    delay(WATER_INTERVAL);
+void WaterPots(){
+  n = 0;
+  for(i = 0; i < NUM; i++){
+    if(pots[i].Watering && pots[i].WaterMute == 0){
+      #ifdef LOGGING
+      Serial.print(i);
+      Serial.print(",WATERING,");
+      Serial.println(pots[i].Value);
+      #endif
 
-    UpdateValue();
-  } while(SENSOR_LOW < pots[i].Value);
-  Serial.print(i);
-  Serial.print(",WATERDONE,");
-  Serial.println(pots[i].Value);
+      digitalWrite(pots[i].PowerRelay, LOW);
+      delay(WATER_DELAY);
+      digitalWrite(pots[i].PowerRelay, HIGH);
+      
+      pots[i].WaterMute = WATER_MUTE; //Mute the watering
+      
+      n++;
+    }
+  }
+}
+
+void DelayRead(){
+  n *= WATER_DELAY;
+  n *= -1;
+  n += READ_INTERVAL;
+  delay(n);
 }
 
 void setup(){
@@ -109,17 +126,19 @@ void setup(){
     pinMode(pots[i].PowerSensor, OUTPUT);
     pinMode(pots[i].DataSensor, INPUT);
     pinMode(pots[i].PowerRelay, OUTPUT);
+    
+    digitalWrite(pots[i].PowerRelay, HIGH);
 
     pots[i].Value = 0;
+    pots[i].WaterMute = 0;
+    pots[i].Watering = false;
   }
 }
 
 void loop(){
   UpdateValues();
   NeedWater();
-  if(i == NO_WATER_NEED){
-    delay(READ_INTERVAL);
-  }else{
-    WaterPot();
-  }
+  WaterPots();
+  DelayRead();
+  
 }
